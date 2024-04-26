f4ec14468df183612e01cfff2c5f828c457cf998
Chinmay Pandit <cpandit@adobe.com>
2024-04-26 23:21:35 +0530

    h1 change

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,7 +33,9 @@ function buildHeroBlock(main) {
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+   // section.append(buildBlock('hero', { elems: [picture, h1] }));
+   section.append(buildBlock('hero', { elems: [picture] }));
+   section.append(h1);
     main.prepend(section);
   }
 }
